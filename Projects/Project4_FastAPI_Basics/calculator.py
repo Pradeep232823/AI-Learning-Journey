9cdc94d8b7c57ee50444ab7a2e662ce6bce1653b
Yellam <naninani88868@gmail.com>
2026-08-16 12:21:30 +0530

    FastAPI Basics Practice commit

---
+++ Projects/Project4_FastAPI_Basics/calculator.py
@@ -0,0 +1,22 @@
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def power(a, b):
+    return math.pow(a, b)
+
+def percentage(a, b):
+    return (a / b) * 100
+
+def square_root(a):
+    return math.sqrt(a)
---
+++ Projects/Project4_FastAPI_Basics/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+import calculator
+
+app = FastAPI()
+
+class CalculateRequest(BaseModel):
+    operation : str
+    a: float
+    b: float | None = None
+
+@app.post("/calculate")
+def calculate_request(request : CalculateRequest):
+
+    requires_b = ["add", "subtract", "multiply", "divide", "power", "percentage"]
+    
+    if request.operation in requires_b and request.b is None:
+        raise HTTPException(
+            status_code=400,
+            detail=f"The '{request.operation}' operation requires a 'b' value."
+        )
+        
+    if request.operation in ["divide", "percentage"] and request.b == 0:
+        raise HTTPException(
+            status_code=400,
+            detail="Cannot divide or calculate percentage by zero."
+        )
+    
+    total = 0
+
+    match request.operation:
+        case "add":
+            total = calculator.add(request.a, request.b)
+        case "subtract":
+            total = calculator.subtract(request.a, request.b)
+        case "multiply":
+            total = calculator.multiply(request.a, request.b)
+        case "divide":
+            total = calculator.divide(request.a, request.b)
+        case "power":
+            total = calculator.power(request.a, request.b)
+        case "percentage":
+            total = calculator.percentage(request.a, request.b)
+        case "square_root":
+            if request.a < 0:
+                raise HTTPException(
+                    status_code=400,
+                    detail="Cannot calculate the square root of a negative number."
+                )
+            total = calculator.square_root(request.a)
+        case _:
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid operation"
+            )
+    return {"operation" : request.operation, "result" : total}
